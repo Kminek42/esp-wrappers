ea27c9aea04dc886b69b0c15e762ffc78038e65b
Kminek42 <k.kminkowski@gmail.com>
2025-08-15 16:46:11 +0200

    fix hysteresis logic

--- potentiometer_manager.c
+++ potentiometer_manager.c
@@ -4,9 +4,9 @@
 
 float* readings;
 uint16_t readings_num;
-float histeresis;
+float hysteresis;
 
-void potentiometer_manager_init(uint16_t potentiometer_num, float histeresis) {
+void potentiometer_manager_init(uint16_t potentiometer_num, float hysteresis) {
     readings = (float*)malloc(potentiometer_num * sizeof(float));
     readings_num = potentiometer_num;
 }
@@ -15,8 +15,8 @@ void potentiometer_manager_monitor_potentiometers(float* raw_readings, void(*not
     for (uint16_t i = 0; i < readings_num; i++) {
         float raw_reading = raw_readings[i];
         float last_reading = readings[i];
-        if (ABS(raw_reading - last_reading) > histeresis) {
-            last_reading = raw_reading;
+        if (ABS(raw_reading - last_reading) > hysteresis) {
+            readings[i] = raw_reading;
             notify_change(i, last_reading);
         }
     }
--- potentiometer_manager.h
+++ potentiometer_manager.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "stdint.h"
 
-void potentiometer_manager_init(uint16_t potentiometer_num, float histeresis);
+void potentiometer_manager_init(uint16_t potentiometer_num, float hysteresis);
 
 void potentiometer_manager_monitor_potentiometers(float* raw_readings, void(*notify_change)(uint16_t potentiometer_id, float reading));
